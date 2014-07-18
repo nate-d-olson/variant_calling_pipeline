97707611e33e2992df8bf35b6d7a29a730ce728d
Nate Olson <nolson@nist.gov>
2014-07-18 11:58:48 -0400

    revised validation pipeline

---
+++ validation.config.groovy
@@ -0,0 +1,19 @@
+//Project directory
+PRJ_HOME="/media/nolson/second/mirror/micro_rm_dev"
+// Set location of you reference files here (see below for the files required)
+REFBASE="$PRJ_HOME/references"
+
+// Set a good location for storing large temp files here (probably not /tmp)
+TMPDIR="$PRJ_HOME/tmp"
+
+// Set location of executables
+BIN="$PRJ_HOME/bin"
+
+// Log data from various tools will appear in here
+LOG="pipeline.log"
+
+// Number of threads
+n=7
+
+// java heap size
+JHEAP="-Xmx8g"
