2a1508ed92c980524f90fafb1464b2479f34825f
Nate Olson <nolson@nist.gov>
2014-08-14 10:20:13 -0400

    dekstop updates

--- pipeline_denovo.groovy
+++ pipeline_denovo.groovy
@@ -4,13 +4,13 @@
 // 
 ////////////////////////////////////////////////////////////
 
+@transform("")
 de_novo = {
 	exec """
-		perl a5_/home/ubuntu/GMI_bioinf/a5_miseq_linux_20140604/bin/a5_pipeline.pl $inputs.fastq.gz
-		$output
+		perl /home/ubuntu/GMI_bioinf/a5_miseq_linux_20140604/bin/a5_pipeline.pl $inputs.fastq $input.prefix
 	"""
 }
 
 run {
-	"%_L001_R*_001" * de_novo
+	"%_L001_R*_001" * [de_novo]
 }
